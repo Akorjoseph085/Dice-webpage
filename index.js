60ca02d968452b2aea13c80568a7df5f9e9fd2c0
joseph akor <122928305+Akorjoseph085@users.noreply.github.com>
2023-11-18 12:37:05 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+//alert("hello");
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage = "dice" +randomNumber1 + ".png";
+var randomImages = "images/"+ randomDiceImage;
+var image1 = document.querySelectorAll("img")[0].setAttribute("src", randomImages);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage = "dice" +randomNumber2 + ".png";
+var randomImages = "images/"+ randomDiceImage;
+var image2 = document.querySelectorAll("img")[1].setAttribute("src", randomImages);
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 won";
+}else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 2 won";
+}else{
+    document.querySelector("h1").innerHTML = "draw";
+}
